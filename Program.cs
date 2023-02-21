07993ea755e638252385f2dda80790d229c57288
Lunarien <matyas1587@seznam.cz>
2023-02-21 22:36:18 +0100

    first release

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Data.Common;
+using System.Text;
+using System.Collections.Generic;
 using System.Globalization;
-using PeterO.Numbers;
 
 
 namespace Lunarien_s_Mental_Math_Trainer
@@ -11,10 +11,10 @@ namespace Lunarien_s_Mental_Math_Trainer
         public class DigitCode
         {
             //variables that store digit amounts of X and Y, the operation and optionally the decimal digits
-            public int digitsX;
-            public int digitsY;
-            public char operation;
-            public int decimals;
+            public int DigitsX;
+            public int DigitsY;
+            public char Operation;
+            public int Decimals;
             public void Get()
             {                
                 char[] numbers = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
@@ -25,13 +25,13 @@ namespace Lunarien_s_Mental_Math_Trainer
                 while (true)
                 {
                     string usrDigitCode = Console.ReadLine();
-                    if (usrDigitCode.Length == 3) //then check if the first and the third symbol are numbers and the second is an operation.
+                    if (usrDigitCode.Length == 3 && !usrDigitCode.Contains('/') && !usrDigitCode.Contains('R')) //then check if the first and the third symbol are numbers and the second is an operation.
                     {
                         if ((numbers.Contains(usrDigitCode[0])) & (operations.Contains(usrDigitCode[1])) & (numbers.Contains(usrDigitCode[2])))
                         {
-                            digitsX = int.Parse(usrDigitCode[0].ToString());
-                            operation = usrDigitCode[1];
-                            digitsY = int.Parse(usrDigitCode[2].ToString());
+                            DigitsX = int.Parse(usrDigitCode[0].ToString());
+                            Operation = usrDigitCode[1];
+                            DigitsY = int.Parse(usrDigitCode[2].ToString());
                             return;
                         }
                         else
@@ -41,10 +41,10 @@ namespace Lunarien_s_Mental_Math_Trainer
                     {
                         if (numbers.Contains(usrDigitCode[0]) & operations.Contains(usrDigitCode[1]) & numbers.Contains(usrDigitCode[2]) & usrDigitCode[3] == '.' & numbers.Contains(usrDigitCode[4]))
                         {
-                            digitsX = int.Parse(usrDigitCode[0].ToString());
-                            operation = usrDigitCode[1];
-                            digitsY = int.Parse(usrDigitCode[2].ToString());
-                            decimals = int.Parse(usrDigitCode[4].ToString());
+                            DigitsX = int.Parse(usrDigitCode[0].ToString());
+                            Operation = usrDigitCode[1];
+                            DigitsY = int.Parse(usrDigitCode[2].ToString());
+                            Decimals = int.Parse(usrDigitCode[4].ToString());
                             return;
                         }
                         else
@@ -72,29 +72,62 @@ namespace Lunarien_s_Mental_Math_Trainer
             }
             
         }
+        public static ulong RandomUlong(ulong bottom, ulong top)
+        {
+            Random randomness = new();
+            ulong rangeSize = top - bottom;
+
+            byte[] buf = new byte[8];
+            randomness.NextBytes(buf);
+            ulong result = (BitConverter.ToUInt64(buf, 0)%rangeSize)+bottom;
+            return result;
+        }
         static void Main(string[] args)
         {
+            IFormatProvider ifp;
+            ifp = new CultureInfo("en-US");
+            Console.ForegroundColor = ConsoleColor.White;
             // 1) get digit code
             DigitCode usrDC = new();
             usrDC.Get();
             Console.Clear();
-            // 2) make random numbers corresponding to the number of digits in the digit code
-            Random randomness = new();
             while (true)
             {
-                int xRangeBottom = (int)Math.Round(Math.Pow(10, usrDC.digitsX - 1), 0); //the bottom of the range for X. it is 10^(DigitsX - 1).
-                int xRangeTop = (int)Math.Round(Math.Pow(10, usrDC.digitsX), 0); //the top of the range for X. it is 10^DigitsX
-                int x = randomness.Next(xRangeBottom, xRangeTop);
+                // 2) make random numbers corresponding to the number of digits in the digit code
+                ulong xRangeBottom = Convert.ToUInt64(Math.Round(Math.Pow(10, usrDC.DigitsX - 1), 0)); //the bottom of the range for X. it is 10^(DigitsX - 1).
+                ulong xRangeTop = Convert.ToUInt64(Math.Round(Math.Pow(10, usrDC.DigitsX), 0)); //the top of the range for X. it is 10^DigitsX
+                ulong x = RandomUlong(xRangeBottom, xRangeTop);
 
-                int yRangeBottom = (int)Math.Round(Math.Pow(10, usrDC.digitsY - 1), 0);
-                int yRangeTop = (int)Math.Round(Math.Pow(10, usrDC.digitsY), 0);
-                int y = randomness.Next(yRangeBottom, yRangeTop);
+                ulong yRangeBottom = Convert.ToUInt64(Math.Round(Math.Pow(10, usrDC.DigitsY - 1), 0));
+                ulong yRangeTop = Convert.ToUInt64(Math.Round(Math.Pow(10, usrDC.DigitsY), 0));
+                ulong y = RandomUlong(yRangeBottom, yRangeTop);
+
                 // 3) make a problem with the random numbers and the operation
-                string problem = x.ToString() + usrDC.operation + y.ToString();
+                string problem;
+                if (usrDC.Operation == '^')
+                {
+                    problem = x.ToString() + usrDC.Operation + usrDC.DigitsY.ToString();
+                }
+                else if (usrDC.Operation == 'R')
+                {
+                    if (usrDC.DigitsX == 2)
+                    {
+                        problem = '√' + y.ToString();
+                    }
+                    else
+                    {
+                        problem = usrDC.DigitsX.ToString() + '√' + y.ToString();
+                    }
+                    
+                }
+                else 
+                {
+                    problem = x.ToString() + usrDC.Operation + y.ToString();
+                }
                 // 3.5) precompute the correct solution
-                int? intResult = null;
+                ulong? intResult = null;
                 decimal? decResult = null;
-                switch (usrDC.operation)
+                switch (usrDC.Operation)
                 {
                     case '+':
                         intResult = x + y;
@@ -109,45 +142,68 @@ namespace Lunarien_s_Mental_Math_Trainer
                         intResult = x * y;
                         break;
                     case '/':
-                        decResult = Math.Round((decimal)(x / y), usrDC.decimals, MidpointRounding.ToZero);
+                        decResult = Math.Round((decimal)(Convert.ToDecimal(x) / Convert.ToDecimal(y)), usrDC.Decimals, MidpointRounding.ToZero);
                         break;
                     case '^':
-                        intResult = (int)Math.Pow(x, y);
+                        intResult = (ulong)Math.Pow(x, usrDC.DigitsY);
                         break;
                     case 'R':
-                        decResult = Math.Round((decimal)Math.Pow(x, 1 / y), usrDC.decimals, MidpointRounding.ToZero);
+                        decResult = Math.Round((decimal)Math.Pow(Convert.ToDouble(y), 1 / Convert.ToDouble(usrDC.DigitsX)), usrDC.Decimals, MidpointRounding.ToZero);
                         break;
                     default:
                         intResult = 0;
                         decResult = 0.0m;
                         break;
                 }
+
                 // 4) ask the user to solve the problem. verify answer. give evaluation (correct/wrong).
                 Console.WriteLine(problem);
                 Console.Write("Your result: ");
                 
                 string usrResult = Console.ReadLine();
-                if (intResult != null) //if the result is an int. in other words, if there is a result that is of type int.
+                if (intResult != null && usrResult != "") //if the result is an int. in other words, if there is a result that is of type int.
                 {
-                    if (int.TryParse(usrResult, out int _))
+                    if (ulong.TryParse(usrResult, out ulong _))
                     {
-                        if (int.Parse(usrResult) == intResult)
-                            Console.WriteLine("Correct");
+                        if (ulong.Parse(usrResult) == intResult)
+                        {
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Correct");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        }
                         else
+                        {
+                            Console.Clear();
                             Console.WriteLine("Wrong, correct was: " + intResult.ToString());
-                    } 
+                        }
+                    }
                     
                 }
-                else if (decResult != null) //if the result is a decimal
+                else if (decResult != null && usrResult != "") //if the result is a decimal
                 {
-                    decimal decUsrResult; //its a fix for something
-                    decimal.TryParse(usrResult, out decUsrResult);
-                    if (decUsrResult == decResult)
+                    
+                    
+                    if (decResult == decimal.Parse(usrResult, CultureInfo.InvariantCulture))
+                    {
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Green;
                         Console.WriteLine("Correct");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }  
                     else
-                        Console.WriteLine("Wrong, correct was: " + decResult.ToString());
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Wrong, correct was: " + decResult?.ToString(ifp));
+                    }
+                        
                 }
-                // 5) repeat 2-4 until the user quits
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Invalid result.");
+                }
+                // 5) repeat 2-4 until the user quits (to be implemented)
             }
         }
     }
